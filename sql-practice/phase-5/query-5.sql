8e360683d1370c9c60d103f684883d12af0d32ba
Prechyious <lemoprechy@gmail.com>
2023-05-14 21:08:13 +0100

    finished practice

---
+++ sql-practice/phase-5/query-5.sql
@@ -0,0 +1,9 @@
+-- Find names of the cats whose owners are both George Beatty and Melynda Abshire, or just George Beatty, or just Melynda Abshire
+-- Your code here
+
+SELECT cats.name FROM cats
+    JOIN cat_owners ON cats.id = cat_owners.cat_id
+    JOIN owners ON cat_owners.owner_id = owners.id
+    WHERE owners.first_name IN ("George", "Melynda") AND owners.last_name IN ("Beatty", "Abshire")
+    GROUP BY cats.name
+    HAVING COUNT(DISTINCT owners.id) >= 1;
